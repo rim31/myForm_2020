636ad8e5b31b27ca8d1b70e8f1361503e94823ee
toto42 <toto42@yopmail.com>
2020-08-26 14:55:22 +0300

    have to refresh graph on clic

--- frontend/src/components/Answers/MyCharts.tsx
+++ frontend/src/components/Answers/MyCharts.tsx
@@ -14,37 +14,17 @@ export default function MyCharts(props: { results: any }) {
   const unstated = StoreContainer.useContainer();
   const [data, setData] = React.useState<any>(
     [
-      // {1: 1, name: "38"},
-      // {1: 2, 2: 1, 3: 1, 4: 3, 5: 2, name: "62"},
-      // {3: 1, name: "64"}
       { name: 'Q - A', a1: 4000, a2: 2400, a3: 2400, a4: 1000, a5: 3000, },
       { name: 'Q - B', a1: 3000, a2: 1398, a3: 2210, a4: 1000, a5: 3000, },
       { name: 'Q - C', a1: 2000, a2: 9800, a3: 2290, a4: 1000, a5: 3000, },
       { name: 'Q - D', a1: 2780, a2: 3908, a3: 2000, a4: 1000, a5: 3000, }
     ]
   )
-  let test: any = {}
 
-  // const countDuplicate = (id: number, arr: any, test: any) => {
-  //   // let test: any = { "name": id };
-  //   // let test: any = {};
-  //   console.log("id test", id, test)
-  //   arr.forEach((x: any) => { res[x] = (res[x] || 0) + 1; });
-  //   // arr.forEach((x: any) => { counts[(JSON.parse(x)).id] = (counts[(JSON.parse(x)).value] || 0) + 1; });
-  //   // arr.forEach((x: any) => { counts[(JSON.parse(x)).value] = (counts[(JSON.parse(x)).value] || 0) + 1; });
-  //   // arr.forEach((x: any) => { counts[(JSON.parse(x)).value] = (counts[(JSON.parse(x)).id] || 0) + 1; });
-  //   arr.forEach((x: any) => { counts[(JSON.parse(x)).value] = (counts[(JSON.parse(x)).id] || 0) + 1; });
-  //   arr.forEach((x: any) => { totalQuestion[(JSON.parse(x)).value] = (totalQuestion[(JSON.parse(x)).id] || 0) + 1; });
-  //   arr.forEach((x: any) => { test[(JSON.parse(x)).value] = (test[(JSON.parse(x)).id] || 0) + 1; });
-  //   // !('name' in test) && (test.name = id)
-
-  //   console.log("COUNT : ", res, counts, test)
-  // }
 
   const countDuplicate = (id: number, arr: any, res: any) => {
     arr.forEach((x: any) => { res[`a${(JSON.parse(x)).value}`] = (res[`a${(JSON.parse(x)).value}`] || 0) + 1; });
   }
-  // JSON parse "{"id":0,"label":"Terrible","answer":1,"question":"How are you feeling at work?","value":1}"
 
   // do a copy of array TODO
   const add = (arr: any, name: number) => {
@@ -57,27 +37,32 @@ export default function MyCharts(props: { results: any }) {
     let xy: any[] = [];
     for (let index = 0; index < d.length; index++) {
       add(xy, d[index].info);
-      let pos: number | undefined = xy.map((e: any) => { return e.name; }).indexOf(d[index].info); //  get position og the row containing {name: id}
+      let pos: number | undefined = xy.map((e: any) => { return e.name; }).indexOf(d[index].info); //  get position of the row containing {name: id}
       if (d[index].info) {
-        !('name' in test) && (test.name = d[index].info)// add {name: id} if doesn't exist
         countDuplicate(d[index].info, d[index].description, xy[pos]);
       }
     }
     return (xy)
   }
 
+  // React.useEffect(() => {
+  //   console.log('from Mycharts', props.results)
+  //   let r: any;
+  //   if (props.results) {
+  //     r = (donnees(props.results));
+  //     setData(r)
+  //   }
+  //   console.log("TTT", r)
+  // }, [])
+
+
   React.useEffect(() => {
-    console.log('from Mycharts', props.results)
     let r: any;
     if (props.results) {
       r = (donnees(props.results));
-      setData(r)
+      setData(r);
     }
-    console.log("TTT", r)
-  }, [])
-  React.useEffect(() => {
-    console.log("data", data);
-  }, [data])
+  }, [props])
 
   return (
     <BarChart
